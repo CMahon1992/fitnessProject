ae95c027cf87f90bb5d2f27b64ff60778721ace4
CMahon1992 <159093179+CMahon1992@users.noreply.github.com>
2024-03-18 22:14:36 -0500

    added dropdown box
    unfortunately, I lost all the data to the previous project so I had to recreate it. To pick up where I left off, I added three new label links titled Browse Routines, Fitness Progress, and Workout Plans. When the user logs in, the browse routines window will be the default window. I added a dropdown list where the user can select a muscle group they will like to focus on from the dropdown list. Once value is selected, it will be compared to the value within the muscle_group column within  browseRoutines table. Once located, it will then display a list of exercise names that tie to the value that was selected, along with the youtube video to learn how to do the movement.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace homeFitnessSite
+﻿namespace WinFormsApp1
 {
-    partial class loginPage
+    partial class Form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,16 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginPage));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.emailText = new System.Windows.Forms.TextBox();
             this.passwordText = new System.Windows.Forms.TextBox();
-            this.email = new System.Windows.Forms.Label();
-            this.passwordLabel = new System.Windows.Forms.Label();
             this.signIn = new System.Windows.Forms.Button();
             this.signUp = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.signOut = new System.Windows.Forms.LinkLabel();
+            this.routineDDB = new System.Windows.Forms.ComboBox();
+            this.routinesLink = new System.Windows.Forms.LinkLabel();
+            this.progressLink = new System.Windows.Forms.LinkLabel();
+            this.workoutLink = new System.Windows.Forms.LinkLabel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.muscleLabel = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,71 +51,67 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Sitka Heading", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(194, 24);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(159, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(568, 68);
+            this.label1.Size = new System.Drawing.Size(484, 47);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to Velocity Fitness";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Welcome To Velocity Fitness";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(260, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "E-Mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(225, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password:";
             // 
             // emailText
             // 
-            this.emailText.Location = new System.Drawing.Point(477, 180);
-            this.emailText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.emailText.Location = new System.Drawing.Point(369, 161);
             this.emailText.Name = "emailText";
-            this.emailText.Size = new System.Drawing.Size(166, 27);
-            this.emailText.TabIndex = 1;
+            this.emailText.Size = new System.Drawing.Size(132, 23);
+            this.emailText.TabIndex = 3;
             // 
             // passwordText
             // 
-            this.passwordText.Location = new System.Drawing.Point(477, 239);
-            this.passwordText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.passwordText.Location = new System.Drawing.Point(369, 223);
             this.passwordText.Name = "passwordText";
-            this.passwordText.Size = new System.Drawing.Size(166, 27);
-            this.passwordText.TabIndex = 2;
-            // 
-            // email
-            // 
-            this.email.AutoSize = true;
-            this.email.BackColor = System.Drawing.Color.Transparent;
-            this.email.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.email.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.email.Location = new System.Drawing.Point(369, 173);
-            this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(94, 32);
-            this.email.TabIndex = 3;
-            this.email.Text = "E-Mail:";
-            // 
-            // passwordLabel
-            // 
-            this.passwordLabel.AutoSize = true;
-            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
-            this.passwordLabel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.passwordLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.passwordLabel.Location = new System.Drawing.Point(336, 236);
-            this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(129, 32);
-            this.passwordLabel.TabIndex = 4;
-            this.passwordLabel.Text = "Password:";
+            this.passwordText.Size = new System.Drawing.Size(132, 23);
+            this.passwordText.TabIndex = 4;
             // 
             // signIn
             // 
-            this.signIn.Location = new System.Drawing.Point(399, 349);
-            this.signIn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.signIn.Location = new System.Drawing.Point(326, 297);
             this.signIn.Name = "signIn";
-            this.signIn.Size = new System.Drawing.Size(86, 31);
+            this.signIn.Size = new System.Drawing.Size(75, 23);
             this.signIn.TabIndex = 5;
             this.signIn.Text = "Sign In";
             this.signIn.UseVisualStyleBackColor = true;
+            this.signIn.Click += new System.EventHandler(this.signIn_Click);
             // 
             // signUp
             // 
-            this.signUp.Location = new System.Drawing.Point(594, 349);
-            this.signUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.signUp.Location = new System.Drawing.Point(460, 297);
             this.signUp.Name = "signUp";
-            this.signUp.Size = new System.Drawing.Size(86, 31);
+            this.signUp.Size = new System.Drawing.Size(75, 23);
             this.signUp.TabIndex = 6;
             this.signUp.Text = "Sign Up";
             this.signUp.UseVisualStyleBackColor = true;
@@ -118,43 +120,113 @@
             // 
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.muscleLabel);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.workoutLink);
+            this.panel1.Controls.Add(this.progressLink);
+            this.panel1.Controls.Add(this.routinesLink);
+            this.panel1.Controls.Add(this.routineDDB);
             this.panel1.Controls.Add(this.signOut);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(914, 600);
+            this.panel1.Size = new System.Drawing.Size(803, 448);
             this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // signOut
             // 
             this.signOut.AutoSize = true;
             this.signOut.BackColor = System.Drawing.Color.Transparent;
-            this.signOut.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.signOut.Location = new System.Drawing.Point(799, 9);
+            this.signOut.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.signOut.Location = new System.Drawing.Point(683, 22);
             this.signOut.Name = "signOut";
-            this.signOut.Size = new System.Drawing.Size(94, 28);
+            this.signOut.Size = new System.Drawing.Size(71, 21);
             this.signOut.TabIndex = 0;
             this.signOut.TabStop = true;
             this.signOut.Text = "Sign Out";
             // 
-            // loginPage
+            // routineDDB
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.routineDDB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.routineDDB.FormattingEnabled = true;
+            this.routineDDB.Items.AddRange(new object[] {
+            "Arms",
+            "Chest",
+            "Legs",
+            "Core",
+            "Full Body"});
+            this.routineDDB.Location = new System.Drawing.Point(353, 252);
+            this.routineDDB.Name = "routineDDB";
+            this.routineDDB.Size = new System.Drawing.Size(121, 23);
+            this.routineDDB.TabIndex = 1;
+            // 
+            // routinesLink
+            // 
+            this.routinesLink.AutoSize = true;
+            this.routinesLink.Location = new System.Drawing.Point(150, 150);
+            this.routinesLink.Name = "routinesLink";
+            this.routinesLink.Size = new System.Drawing.Size(94, 15);
+            this.routinesLink.TabIndex = 3;
+            this.routinesLink.TabStop = true;
+            this.routinesLink.Text = "Browse Routines";
+            // 
+            // progressLink
+            // 
+            this.progressLink.AutoSize = true;
+            this.progressLink.Location = new System.Drawing.Point(368, 150);
+            this.progressLink.Name = "progressLink";
+            this.progressLink.Size = new System.Drawing.Size(91, 15);
+            this.progressLink.TabIndex = 4;
+            this.progressLink.TabStop = true;
+            this.progressLink.Text = "Fitness Progress";
+            // 
+            // workoutLink
+            // 
+            this.workoutLink.AutoSize = true;
+            this.workoutLink.Location = new System.Drawing.Point(589, 150);
+            this.workoutLink.Name = "workoutLink";
+            this.workoutLink.Size = new System.Drawing.Size(79, 15);
+            this.workoutLink.TabIndex = 5;
+            this.workoutLink.TabStop = true;
+            this.workoutLink.Text = "Workout Plan";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(158, 314);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(510, 94);
+            this.listBox1.TabIndex = 6;
+            // 
+            // muscleLabel
+            // 
+            this.muscleLabel.AutoSize = true;
+            this.muscleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.muscleLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.muscleLabel.Location = new System.Drawing.Point(275, 211);
+            this.muscleLabel.Name = "muscleLabel";
+            this.muscleLabel.Size = new System.Drawing.Size(279, 30);
+            this.muscleLabel.TabIndex = 7;
+            this.muscleLabel.Text = "Choose Your Muscle Group";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.signUp);
             this.Controls.Add(this.signIn);
-            this.Controls.Add(this.passwordLabel);
-            this.Controls.Add(this.email);
             this.Controls.Add(this.passwordText);
             this.Controls.Add(this.emailText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "loginPage";
-            this.Text = "Login ";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "Form1";
+            this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -165,13 +237,19 @@
         #endregion
 
         private Label label1;
+        private Label label2;
+        private Label label3;
         private TextBox emailText;
         private TextBox passwordText;
-        private Label email;
-        private Label passwordLabel;
         private Button signIn;
         private Button signUp;
         private Panel panel1;
         private LinkLabel signOut;
+        private LinkLabel workoutLink;
+        private LinkLabel progressLink;
+        private LinkLabel routinesLink;
+        private ComboBox routineDDB;
+        private Label muscleLabel;
+        private ListBox listBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,71 +1,27 @@
-using System;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System;
 
-namespace homeFitnessSite
+
+namespace WinFormsApp1
 {
-    public partial class loginPage : Form
+    public partial class Form1 : Form
     {
         // Connection string to the SQL Server database
         private string connectionString = @"Data Source=GWNC31514\SQLEXPRESS;Initial Catalog=homeExercise;Integrated Security=True";
-
-        public loginPage()
+        public Form1()
         {
             InitializeComponent();
             // Hide panel1
             panel1.Visible = false;
-            // Set the background image and adjust its layout
-            SetBackgroundImage();
             //this should activate the button click event
             signIn.Click += signIn_Click;
             //linkClicked event of the signout LinkLabel
             signOut.LinkClicked += signOut_LinkClicked;
+            //event of the combobox
+            routineDDB.SelectedIndexChanged += routineDDB_SelectedIndexChanged;
         }
 
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SetBackgroundImage()
-        {
-            // Load the background image from file
-            Image backgroundImage = Image.FromFile(@"C:\Users\CMaho\source\repos\fitnessProject\images\homePageImage.jpg");
-
-            // Set the background image and adjust its layout
-            this.BackgroundImageLayout = ImageLayout.Zoom; 
-            this.BackgroundImage = backgroundImage;
-
-            // Call ResizeBackgroundImage to adjust the background image when the form is resized
-            this.Resize += new EventHandler(ResizeBackgroundImage);
-
-            // Call CenterButtons to center the buttons initially
-            CenterButtons();
-        }
-
-        private void ResizeBackgroundImage(object sender, EventArgs e)
-        {
-            // Resize the background image to match the size of the form
-            if (this.BackgroundImage != null)
-            {
-                this.BackgroundImage = new Bitmap(this.BackgroundImage, this.ClientSize.Width, this.ClientSize.Height);
-                CenterButtons(); // Center the buttons when the form is resized
-            }
-        }
-
-        private void CenterButtons()
-        {
-            // Calculate the new position for buttons
-            int buttonWidth = 100; //  button width
-            int buttonHeight = 30; // button height
-
-            // Center SignIn button horizontally
-            signIn.Left = (ClientSize.Width - signIn.Width) / 2;
-            // Place SignUp button below SignIn button with a vertical margin of 20 pixels
-            // signUp.Right = (ClientSize.Width - signUp.Width) / 2;
-            // signUp.Top = signIn.Bottom + 20; // Adjust the margin as needed
-        }
 
         private void signIn_Click(object sender, EventArgs e)
         {
@@ -103,14 +59,47 @@ namespace homeFitnessSite
                         MessageBox.Show("Error: Please check your login credentials.");
                     }
                 }
+            }
 
+        }
+
+        private void routineDDB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Clear the existing items in listBox1
+            listBox1.Items.Clear();
+
+            // Retrieve the selected option from the ComboBox
+            string selectedRoutine = routineDDB.SelectedItem.ToString();
+
+            // Query to fetch exercise names and YouTube links based on the selected option
+            string query = "SELECT exercise_name, youtube FROM browseRoutines WHERE muscle_group = @MuscleGroup";
+
+            // Create and open a connection to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Create a SqlCommand object with the query and connection
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameter to the query to prevent SQL injection
+                    command.Parameters.AddWithValue("@MuscleGroup", selectedRoutine);
+
+                    // Execute the query and read the results
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Iterate through the results and add them to listBox1
+                        while (reader.Read())
+                        {
+                            // Combine exercise name and YouTube link
+                            string exerciseInfo = $"{reader["exercise_name"]} - {reader["youtube"]}";
+                            // Add the combined string to listBox1
+                            listBox1.Items.Add(exerciseInfo);
+                        }
+                    }
+                }
             }
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-        }
-
         private void signOut_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             // Hide panel1
@@ -119,7 +108,9 @@ namespace homeFitnessSite
             emailText.Text = "";
             passwordText.Text = "";
         }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
 
-
+        }
     }
 }
